fd5b017fc0c812fac55ee40b5d8a3e0be39e7709
Casey Whorton <27929027+caseywhorton@users.noreply.github.com>
2023-08-03 22:19:14 -0400

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,4 +1,4 @@
 #!/bash/bin
 
 echo "***** Executing curl command. *****"
-curl -g -o output.json -X GET "https://api.eia.gov/v2/electricity/retail-sales/data?api_key=<api_key>&frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&facets[stateid][]=OH&start=2023-01&end=2023-05&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+curl -g -o electric_consumption.json -X GET "https://api.eia.gov/v2/electricity/retail-sales/data?api_key=<api_key>&frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&facets[stateid][]=OH&start=2023-01&end=2023-05&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
