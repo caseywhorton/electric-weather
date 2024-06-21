a04b9c8eb83e0e0061a3e4921dbee37fd4ba3a13
Casey Whorton <27929027+caseywhorton@users.noreply.github.com>
2024-06-21 11:29:57 -0400

    Delete utils directory

--- utils/db.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas as pd
-
-def load_electricity_data(json_filepath)->pd.DataFrame:
-    """Loads the electricity data to a pandas DataFrame."""
-    f = open(json_filepath)
-    data = json.load(f)
-    f.close()
-    return(pd.DataFrame(data['response']['data']))
